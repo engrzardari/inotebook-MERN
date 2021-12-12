a3e16e3db9d0a374008c263f6888fdf5f2c43132
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-12-12 08:37:08 -0800

    added mongo DB configuration

---
+++ backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+
+// Connect function
+const connectToMongo =()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log('Mongoose connected successfully');
+    })    
+} 
+
+module.exports = connectToMongo;
