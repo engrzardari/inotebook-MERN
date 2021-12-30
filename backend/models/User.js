c3cef40e633c1388445746227ac3c96bd7d4658d
Engr Saddam Zardari <41242658+engrzardari@users.noreply.github.com>
2021-12-30 09:10:50 -0800

    added records in MongoDB

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+  const UserSchema = new Schema({
+    name:{
+        type: String,
+        required : true
+    },
+    email:{
+        type: String,
+        required : true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required : true
+    },
+    phone:{
+        type: String
+    },
+    address:{
+        type: String
+    },
+    date:{
+        type: Date,
+        default : Date.now
+    }
+  });
+
+module.exports = mongoose.model('user',UserSchema);
